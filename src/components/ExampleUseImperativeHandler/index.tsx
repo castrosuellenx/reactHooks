d31cd6d5f41657fcf9399fa33719cdfa90d662e5
Suéllen Castro <castrosuellenx@gmail.com>
2022-04-06 21:13:29 -0300

    initial files

---
+++ src/components/ExampleUseImperativeHandler/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const ExampleUseImperativeHandler: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Title>ExampleUseImperativeHandler</S.Title>
+    </S.Container>
+  );
+};
+
+export default ExampleUseImperativeHandler;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ExampleUseImperativeHandler from './components/ExampleUseImperativeHandler';
+
+const App: React.FC = () => {
+  return <ExampleUseImperativeHandler />;
+};
+
+export default App;
